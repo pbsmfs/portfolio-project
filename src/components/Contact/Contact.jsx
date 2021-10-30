190555f216add33dd9c7f07cdc435be6155ecbce
JuniorTheF <fsmart2006@gmail.com>
2021-10-30 21:33:25 +0300

    step 5 Contact component done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,24 @@
 import './contact.css'
-import { useRef } from 'react'
-
+import { useRef, useState } from 'react'
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-    const formRef = useRef()
-    
+
+const form = useRef();
+const [done, setDone] = useState(false)
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ok3ysug', 'template_8mkv4k9', form.current, 'user_WMuCMQzlDdsB7UILHcLFp')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true)
+          setTimeout(setDone, 5000, false)
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
     return (
         <div className="c"> 
             <div className="c-bg"></div>
@@ -21,17 +35,21 @@ const Contact = () => {
                     <p className="c-desc">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat.
                     </p>
-                    <form>
+                    <form ref={form} onSubmit={sendEmail}>
                         <input type="text" placeholder="Name" name="user_name"/>
                         <input type="text" placeholder="Subject" name="user_subject"/>
                         <input type="text" placeholder="Email" name="user_email"/>
                         <textarea name="user_message" rows="5"></textarea>
-                        <button ref={formRef} onClick={(e) =>{e.preventDefault()}}>Submit</button>
+                        <button>Submit</button>
+                        <div className="c-done">
+                            {done && 'Thank you for your letter.'}
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     )
 }
+
 
 export default Contact
