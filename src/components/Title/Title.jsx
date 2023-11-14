b868340f943ccc1eb4d012d7b12d5c8e9df3eab3
JuniorTheF <fsmart2006@gmail.com>
2023-11-14 22:08:35 +0300

    new version

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,44 +1,51 @@
 import "./title.css"
 import md from "../../images/mend.jpg"
-import bf from "../../images/bosf.png"
+import bf from "../../images/dibil.png"
+import Header from "../Header/Header"
+const Title = () => (<>
 
-const Title = () => {
-    return (
-        <div className="t">
-            <div className="t-left">
-                <div className="t-left-wrapper">
-                    <div className="t-intro">
-                        Hello, my name is
-                    </div>
-                    <div className="t-name">
-                        Fedor
-                    </div>
-                    <div className="t-items">
-                        <div className="t-items-wrapper">
-                            <div className="t-item">
-                                junior programmer
-                            </div>
-                            <div className="t-item">
-                                15y.o.
-                            </div>
-                            <div className="t-item">
-                                from Russia
-                            </div>
+    <Header />
+    <div className="t">
+        <img src={bf} alt="" className="t-img" />
+        <div className="t-left">
+
+            <div className="t-left-wrapper">
+                <div className="t-intro">
+                    Hello, my name is
+                </div>
+                <div className="t-name">
+                    Fedor
+                </div>
+                <div className="t-items">
+                    <div className="t-items-wrapper">
+                        <div className="t-item">
+                            junior programmer
+                        </div>
+                        <div className="t-item">
+                            17y.o.
+                        </div>
+                        <div className="t-item">
+                            from Russia
                         </div>
                     </div>
-                    <div className="t-desc">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </div>
                 </div>
+                <div className="t-desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </div>
+                <iframe className="t-video" width="560" height="315" src="https://www.youtube.com/embed/gwaXYbcZQIs" frameborder="0" allowfullscreen></iframe>
             </div>
-            <div className="t-right">
-                <div className="t-right-wrapper">
-                    <img src={md} alt="" className="t-bg" />
-                    <img src={bf} alt="" className="t-img" />   
-                </div>
+
+        </div>
+        <div className="t-right">
+            <div className="t-right-wrapper">
+                <img src={md} alt="" className="t-bg" />
             </div>
         </div>
-    )
-}
+
+
+        {/* <video src='https://www.youtube.com/watch?v=gwaXYbcZQIs'>xd</video> */}
+    </div>
+</>
+)
 
 export default Title
